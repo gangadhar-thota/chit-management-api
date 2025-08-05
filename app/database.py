9817b62e72660272390375d4854d524aa0ea58f1
Gangadharthota <gangadharthota1@ajrgroup.in>
2025-08-06 00:17:01 +0530

    deployment set up

--- app/database.py
+++ app/database.py
@@ -5,10 +5,11 @@ import os
 
 load_dotenv()
 
-DB_URL = (
-    f"mysql+mysqlconnector://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}"
-    f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}?ssl_ca=./certs/ca.pem"
-)
+# DB_URL = (
+#  f"mysql+mysqlconnector://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}" 
+#  f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}?ssl_ca=./certs/ca.pem"
+#)
+DB_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
